32f5b3f14aad38f7bd558b103817953143524903
DulamaA <114867084+DulamaA@users.noreply.github.com>
2025-03-26 11:21:35 +0100

    feat: add cors

--- Lektion-03/express-refactored/src/index.ts
+++ Lektion-03/express-refactored/src/index.ts
@@ -1,15 +1,17 @@
 import express from "express";
+import cors from "cors";
 const app = express();
 
 // Middleware
 app.use(express.json()); // This specific middleware parses JSON string to Javascript Object
+app.use(cors()); //This makes the Express server accept request from other domain
 
 // Routes
 import todoRouter from "./routes/todos";
 import postRouter from "./routes/posts";
 
-app.use('/todos', todoRouter)
-app.use('/posts', postRouter)
+app.use("/todos", todoRouter);
+app.use("/posts", postRouter);
 
 //Port 3000
 const PORT = 3000;
