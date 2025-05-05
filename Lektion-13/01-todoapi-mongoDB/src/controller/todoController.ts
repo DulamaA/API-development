56c3c3557831f144c476aa710945e9de07e12488
DulamaA <114867084+DulamaA@users.noreply.github.com>
2025-05-05 13:13:17 +0200

    add all endpoints med mongodb/mongoose

--- Lektion-13/01-todoapi-mongoDB/src/controller/todoController.ts
+++ Lektion-13/01-todoapi-mongoDB/src/controller/todoController.ts
@@ -20,11 +20,12 @@ export const fetchTodo = async (req: Request, res: Response) => {
   const id = req.params.id;
 
   try {
-    // if (!todo) {
-    //   res.status(404).json({ error: "Todo not found" });
-    //   return;
-    // }
-    // res.json(todo);
+    const todo = await Todo.findById(id);
+    if (!todo) {
+      res.status(404).json({ error: "Todo not found" });
+      return;
+    }
+    res.json(todo);
   } catch (error: unknown) {
     const message = error instanceof Error ? error.message : "Unknown error";
     res.status(500).json({ error: message });
@@ -39,7 +40,10 @@ export const createTodo = async (req: Request, res: Response) => {
   }
 
   try {
-    //res.status(201).json({ message: "Todo created", id: result.insertId });
+    const done = req.body.done ?? false; // Default to false if not provided
+    const newTodo = new Todo({ content, done });
+    const savedTodo = await newTodo.save();
+    res.status(201).json(savedTodo);
   } catch (error: unknown) {
     const message = error instanceof Error ? error.message : "Unknown error";
     res.status(500).json({ error: message });
@@ -61,6 +65,18 @@ export const updateTodo = async (req: Request, res: Response) => {
     //   return;
     // }
     // res.status(201).json({ message: "Todo updated", id });
+    const updated = await Todo.findByIdAndUpdate(
+      id,
+      { content, done },
+      { new: true, runValidators: true }
+    );
+
+    if (!updated) {
+      res.status(404).json({ error: "Todo not found" });
+      return;
+    }
+
+    res.status(200).json(updated);
   } catch (error: unknown) {
     const message = error instanceof Error ? error.message : "Unknown error";
     res.status(500).json({ error: message });
@@ -71,11 +87,13 @@ export const deleteTodo = async (req: Request, res: Response) => {
   const id = req.params.id;
 
   try {
-    //  {
-    //   res.status(404).json({ error: "Todo not found" });
-    //   return;
-    // }
-    // res.json({ message: "Todo deleted" });
+    const deleted = await Todo.findByIdAndDelete(id);
+    if (!deleted) {
+      //  {
+      res.status(404).json({ error: "Todo not found" });
+      return;
+    }
+    res.json({ message: "Todo deleted" });
   } catch (error: unknown) {
     const message = error instanceof Error ? error.message : "Unknown error";
     res.status(500).json({ error: message });
